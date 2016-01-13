d3d1ccf12fee458452bbf151e876d07a7b618177
Erik Brown <erikdbrown@gmail.com>
2016-01-13 12:10:59 -0800

    restructured models and controllers into separate folders.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,81 +3,99 @@ var User = require('../models/userModel.js');
 var session = require('express-session');
 var jwt = require('jwt-simple');
 
+//findone is the actual mongoose method, and it is being called on the User model provided as the second arg. 
 var findUser = Q.nbind(User.findOne, User);
+// create user is a method that uses the create mongoose method to instantiate a new User model
 var createUser = Q.nbind(User.create, User);
 
+//export methods that utilize the Q methods above. 
 module.exports = {
   
   create: function(req, res, next) {
-    console.log(req.body);
-
     findUser({ username: req.body.username })
-    .then(function (user) {
+	  .then(function (user) {
+	    console.log('user is', user)
       if (user) {
-        //should refactor this later not to return an error but to check the session. 
-        next(new Error('User already in the database'));
-      } else {
+	      //should refactor this later not to return an error but to check the session. 
+		    next(new Error('User already in the database'));
+		  } else {
+	      console.log('we created a user')
         return createUser({
-          username: req.body.username,
-          password: req.body.password,
-          displayName: req.body.displayName
+		      username: req.body.username,
+		      password: req.body.password
+		    });
+		  }
+	  });
+  }, 
+
+  signin: function(req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    //call findUser, which is the mongoose method findone, which will query for the user from post req
+    console.log(req.body);
+    findUser({
+      username: username
+    })
+    //when done, if found...
+    .then(function(user) {
+      if (user) {
+        //...user exists. Check their password.
+        //save database password
+        var dbPassword = user.get('password');
+        //if input password equals db password
+        if (dbPassword === password) {
+          console.log('Congrats. You got in')
+          // create token to send back for auth
+          var token = jwt.encode(user, 'secret');
+          //the big question here is exactly how to handle a redirect. know that res.redirect and res.json end
+          //a response, so redirection may have to be handled on client side. 
+          res.json({
+            token: token, username: username
+          });
+        //password does not match
+        } else {
+          console.log('Less congrats. You are redirected')
+          //redirect to login
+          res.redirect('/signin'); 
+        }
+      //user not found
+      } else {
+        console.log('Bruh. Sign up')
+        //redirect to sign up
+        res.redirect('/register'); 
+      }
+    });
+  }, 
+
+  newUser: function(req, res, next) {
+    //save input username
+    var username = req.body.username;
+    //save input password
+    var password = req.body.password;
+    //create a new user with the request username and password
+    findUser({username: username})
+    .then(function(user) {
+      //if user exists...
+      if(user) {
+        //...throw error as username already stored in db. 
+        next(new Error('username already exists!'));
+      } else {
+        //otherwise create a user with the provided username and password
+        return createUser({
+          username: username,
+          password: password
         });
       }
     })
     .then(function(user) {
-      var token = jwt.encode(user.username, 'hrPenguins');
-      res.json({
-        token: token, username: user.username
-      });
-    })
-    .fail(function (error) {
-      next(error);
-    });
-  }, 
-
-  signin: function(req, res, next) {
-    var password = req.body.password;
-    //call findUser, which is the mongoose method findone, which will query for the user from post req
-    findUser({
-      username: req.body.username
-    })
-    //when done, if found...
-    .then(function(user) {
-      if (!user) {
-        res.sendStatus(401);
-      } else {
-        return user.comparePassword(password)
-          .then(function(found) {
-            if (found) {
-              var token = jwt.encode(user.username, 'hrPenguins');
-              res.json({
-                token: token
-              });
-            } else {
-              res.sendStatus(401);
-            }
-          })
-      }
-    })
+      var token = jwt.encode(user, 'secret');
+      //res.redirect('/browse');
+      res.json({token: token});
+    }); 
   },
 
   getUser: function(req, res, next) {
     // returns a user from the database
-    var username = req.username;
-    console.log(username);
-    findUser({ username: username })
-    .then(function(user) {
-      if (!user) {
-        res.sendStatus(404);
-      } else {
-        res.json({
-          username: user.username,
-          foodTokens: user.foodTokens,
-          displayName: user.displayName,
-          rating: user.rating
-        })
-      }
-    })
   },
 
   removeUser: function(req, res, next) {
--- server/models/mealModel.js
+++ server/models/mealModel.js
@@ -1,30 +1,21 @@
 var mongoose = require('mongoose');
-var User =  require('./userModel.js');
-var Feedback =  require('./feedbackModel.js');
-
 
 var MealSchema = mongoose.Schema({
-  // _id: populated automatically by mongoDB
   imgUrl: String, // TODO: store picure URL 
   description: String,
   title: String,
-  ingredients: [String],
-  _creator: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' }, // TODO: need to pull the _id from Users schema
-  consumers: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Users' } ],  // in query, this will be populated
-  date_available: { type: Date, default: Date.now }, // TODO: check that this is correct
+  ingredients: Array,
+  _creator: String, // TODO: need to pull this from Users schema
+  consumers: Array,
+  date_available: {type: Date, default: Date.now}, // TODO: check that this is correct
   portions: Number,
-  tags: [String], // in query, this will be populated
-  rating: { type: Number, default: 0 } // need to write a 'query with options' http://mongoosejs.com/docs/populate.html
+  tags: Array,
+  feedback: Array,
+  overall: Number
 },
 { 
   timestamps: true
 });
-
-// MealSchema.pre('save', function(next) {
-//   var meal = this;
-//   meal.save();
-
-// })
 
 var Meal = mongoose.model('Meals', MealSchema);
 
