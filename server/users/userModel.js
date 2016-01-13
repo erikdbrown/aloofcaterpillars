b7d62cab41c65c52c45715a55f4b40e1f1c02e49
Erik Brown <erikdbrown@gmail.com>
2016-01-12 21:23:48 -0800

    added more routes and created feedback model and controller. placeholder for some callback controller functions

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
   username: String,
-  password: String
+  password: String,
+  foodTokens: Number,
+  displayName: String
 });
 
 var User = mongoose.model('User', userSchema);
