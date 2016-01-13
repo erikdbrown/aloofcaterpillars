bdd10c2225703d8a8653fd51328376a1814d1887
Erik Brown <erikdbrown@gmail.com>
2016-01-13 13:31:24 -0800

    added references to ObjectIDs on each model

--- server/models/feedbackModel.js
+++ server/models/feedbackModel.js
@@ -1,14 +1,20 @@
 var mongoose = require('mongoose');
+var Meal =  require('./mealModel.js');
+var User =  require('./userModel.js');
 
-var feedbackSchema = mongoose.Schema({
-  user_creator: String, // TODO: needs to reference the creator's ObjectID
-  user_eater: String, // TODO: needs to reference the eater's ObjectID
+var FeedbackSchema = mongoose.Schema({
+  user_creator: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' }, // TODO: needs to reference the creator's ObjectID
+  user_eater: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' }, // TODO: needs to reference the eater's ObjectID
   ratingOne: { type: Number, default: 0 }, // TODO: check in on what things we're rating
   ratingTwo: { type: Number, default: 0 },
   ratingThree: { type: Number, default: 0 },
-  meal: String // TODO: needs to reference the meal's ObjectID
+  meal: { type: mongoose.Schema.Types.ObjectId, ref: 'Meals' } // TODO: needs to reference the meal's ObjectID
 });
 
-var Feedback = mongoose.model('Feedback', feedbackSchema);
+FeedbackSchema.pre('save', function(next) {
+
+})
+
+var Feedback = mongoose.model('Feedback', FeedbackSchema);
 
 module.exports = Feedback;
