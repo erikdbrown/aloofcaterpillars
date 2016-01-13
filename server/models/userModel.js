db3b966ce565d746031052ca1b61c92268bafc43
Erik Brown <erikdbrown@gmail.com>
2016-01-13 13:31:24 -0800

    added references to ObjectIDs on each model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,27 +1,30 @@
 var mongoose = require('mongoose');
+var Feedback =  require('./feedbackModel.js');
+var Meal =  require('./mealModel.js');
 
-var userSchema = mongoose.Schema({
+var UserSchema = mongoose.Schema({
+  // _id: automatically populated by MongoDB
   username: String,
   password: String,
   foodTokens: { type: Number, default: 0 },
   displayName: String,
   rating: { type: Number, default: 0 },
   ratings: {
-    received: Array,
-    delivered: Array 
+    received: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Feedback' } ],
+    delivered: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Feedback' } ] 
   },
   meals: {
     created: {
-      current: Array,
-      past: Array
+      current: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Meals' } ],
+      past: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Meals' } ]
     },
     eating: {
-      current: Array,
-      past: Array
+      current: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Meals' } ],
+      past: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Meals' } ]
     }
   }
 });
 
-var User = mongoose.model('Users', userSchema);
+var User = mongoose.model('Users', UserSchema);
 
 module.exports = User; 
