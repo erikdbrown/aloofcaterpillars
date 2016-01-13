a37df9e7371576fbe5fae4ff27441e448c6aed80
Erik Brown <erikdbrown@gmail.com>
2016-01-12 19:07:52 -0800

    modified routes. created Feedback schema and controller

---
+++ server/feedback/feedbackController.js
@@ -0,0 +1,12 @@
+var Feedback = require('./feedbackModel');
+
+module.exports = {
+  retrieveFeedback: function(req, res, next) {
+    // returns the ratings for the meals from a specific user
+  },
+
+  addFeedback: function(req, res, next) {
+    // adds feedback about a specific meal from one user
+  }
+
+};
