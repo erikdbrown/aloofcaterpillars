6bd393a262330cd019fb8758c21c2b09b3bb4035
Erik Brown <erikdbrown@gmail.com>
2016-01-12 19:07:52 -0800

    modified routes. created Feedback schema and controller

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,7 @@
 // links to controllers
 var userController = require('../users/userController.js');
 var mealController = require('../meals/mealController.js');
+var feedbackController = require('../feedback/feedbackController.js');
 
 // Middleware. Add below as needed
 
@@ -12,17 +13,17 @@ module.exports = function(app, express) {
 
   // creating, retreiving, and removing meals
   app.get('/boorish/meals', mealController.allMeals); // get all meals
-  app.post('/boorish/meals', mealController.create); // create a new meal
+  app.post('/boorish/meals', mealController.createMeal); // create a new meal
   app.delete('/boorish/meals/:id', mealController.deleteMeal) // TODO: write controller to delete meal
 
   // adding, retrieving, and deleting a user's meals
   app.get('/boorish/meals/user/:id', checkUser, mealController.userMeals) // TODO: write a controller to GET a specific user's meals
-  app.post('/boorish/meals/user/:id', checkUser, mealController.addMeal) // TODO: write a controller to add a meal
-  app.delete('/boorish/meals/user/:id', mealController.deleteMeal) // TODO: write controller to delete meal from user's meals
+  app.post('/boorish/meals/user/:id', checkUser, mealController.addMealToUser) // TODO: write a controller to add a meal
+  app.delete('/boorish/meals/user/:id', mealController.deleteMealFromUser) // TODO: write controller to delete meal from user's meals
 
   // retrieving and adding feedback on individual meals
-  app.get('/boorish/feedback/meals/:id', feedbackController.retrieveFeedback) // TODO: write controller to retrievie feedback on a meal
-  app.post('/boorish/feedback/meals/:id', feedbackController.addFeedback) // TODO: write controller to add feedback to a meal
+  app.get('/boorish/feedback/users/:id', feedbackController.retrieveFeedback) // TODO: write controller to retrievie feedback on a meal
+  app.post('/boorish/feedback/users/:id', feedbackController.addFeedback) // TODO: write controller to add feedback to a meal
 
   // retrieving and deleting a specific user
   app.get('/boorish/users/:id', userController.getUser)
