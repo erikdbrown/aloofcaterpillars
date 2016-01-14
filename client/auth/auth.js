fb12ca4cdb3d74933b5de0bef878e6fd090e17ab
giselakayy <giselakottmeier@gmail.com>
2016-01-13 21:01:31 -0800

    resolved auth merge conflict

--- client/auth/auth.js
+++ client/auth/auth.js
@@ -33,7 +33,7 @@ angular.module('auth', [])
   $scope.signin = function () {
     Auth.signin($scope.user)
       .then(function (user) {
-<<<<<<< 6f7ffca493e5760f3acf4207327913d925496f23
+
         $window.localStorage.setItem('com.oneApp', user.authToken)
         $window.localStorage.setItem('com.oneAppUser', user.username);
         $window.localStorage.setItem('com.oneAppBalance', user.foodTokens);
@@ -55,10 +55,12 @@ angular.module('auth', [])
       });
   }
 
-  // $scope.signout = function() {
-  //   console.log('clicked')
-  //   $window.localStorage.setItem('com.oneApp', {})
-  //   $window.localStorage.setItem('com.oneAppUser', "")
-  //   $location.path('/signin')
-  // };
+  $scope.signout = function() {
+
+    $window.localStorage.removeItem('com.oneApp')
+    $window.localStorage.removeItem('com.oneAppUser')
+    $window.localStorage.removeItem('com.oneAppBalance')
+    
+    $location.path('/signin')
+  };
 });
