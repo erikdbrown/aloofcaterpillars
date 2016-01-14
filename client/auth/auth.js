adac789bb28ed3c89a1f65be2a893e219ce813ab
giselakayy <giselakottmeier@gmail.com>
2016-01-14 14:41:13 -0800

    Deleted Unneccessary local storage saved items. Now only user token saved. Additionally added isAuth to check if a user is logged in and deleteAcc.

--- client/auth/auth.js
+++ client/auth/auth.js
@@ -16,8 +16,7 @@ angular.module('auth', [])
     Auth.signup($scope.user)
       .then(function (user) {
         if(!user.error){
-          $window.localStorage.setItem('com.oneApp', user.token);
-          $window.localStorage.setItem('com.oneAppID', user.id);
+          $window.localStorage.setItem('com.oneApp', user.authToken);
           $location.path('/browse');
         }
         else{
@@ -31,17 +30,11 @@ angular.module('auth', [])
   };
 
   $scope.signin = function () {
+
     Auth.signin($scope.user)
       .then(function (user) {
-
-        $window.localStorage.setItem('com.oneApp', user.authToken)
-        $window.localStorage.setItem('com.oneAppUser', user.username);
-        $window.localStorage.setItem('com.oneAppBalance', user.foodTokens);
-        $location.path('/browse');
-
         if(!user.error){
           $window.localStorage.setItem('com.oneApp', user.authToken)
-          $window.localStorage.setItem('com.oneAppID', user.id);
           $scope.username = user.name;
           $scope.lunchboxes = user.foodTokens;
           $location.path('/browse');
@@ -53,14 +46,36 @@ angular.module('auth', [])
       .catch(function (error) {
         console.error(error);
       });
+
   }
 
   $scope.signout = function() {
-
     $window.localStorage.removeItem('com.oneApp')
-    $window.localStorage.removeItem('com.oneAppUser')
-    $window.localStorage.removeItem('com.oneAppBalance')
-    
     $location.path('/signin')
   };
+
+  $scope.deleteAcc = function (password){
+    // password should look like {password: "blah"}
+    Auth.deleteAcc(password)
+    .then(function (error){
+      if(!error){
+        $scope.signout();
+        // SUCCESS
+      }
+      else {
+        $scope.error = error;
+      }
+    })
+  };
+
+  $scope.isAuth = function (){
+    Auth.isAuth().then(function (resp){
+      if(resp){
+        return true;
+      }
+      else{
+        return false;
+      }
+    })
+  }
 });
