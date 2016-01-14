244e7444e30b458758f1c2e0b49d8b8e94105f07
Erik Brown <erikdbrown@gmail.com>
2016-01-13 21:23:27 -0800

    user can create a new account

--- server/models/mealModel.js
+++ server/models/mealModel.js
@@ -8,18 +8,16 @@ var MealSchema = mongoose.Schema({
   imgUrl: String, // TODO: store picure URL 
   description: String,
   title: String,
-  ingredients: String,
-  _creator: String,  // { type: mongoose.Schema.Types.ObjectId, ref: 'Users' }, // TODO: need to pull the _id from Users schema
-  consumers: String,  // in query, this will be populated
-  date_available: String, // { type: Date, default: Date.now }, // TODO: check that this is correct
-  portions: String,
-  tags: String, // in query, this will be populated
-  // feedback: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', default: [] }, // in query, this will be populated
-  // overall: Number // need to write a 'query with options' http://mongoosejs.com/docs/populate.html
-// }
-// ,
-// { 
-  // timestamps: true
+  ingredients: [String],
+  _creator: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' }, // TODO: need to pull the _id from Users schema
+  consumers: [String],  // in query, this will be populated
+  date_available: { type: Date, default: Date.now }, // TODO: check that this is correct
+  portions: Number,
+  tags: [String], // in query, this will be populated
+  rating: { type: Number, default: 0 } // need to write a 'query with options' http://mongoosejs.com/docs/populate.html
+},
+{ 
+  timestamps: true
 });
 
 // MealSchema.pre('save', function(next) {
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -6,23 +6,23 @@ var UserSchema = mongoose.Schema({
   // _id: automatically populated by MongoDB
   username: String,
   password: String,
-  foodTokens: { type: Number, default: 0 },
   displayName: String,
-  rating: { type: Number, default: 0 },
-  ratings: {
-    received: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Feedback' } ],
-    delivered: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Feedback' } ] 
-  },
-  meals: {
-    created: {
-      current: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Meals' } ],
-      past: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Meals' } ]
-    },
-    eating: {
-      current: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Meals' } ],
-      past: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Meals' } ]
-    }
-  }
+  foodTokens: { type: Number, default: 0 },
+  rating: { type: Number, default: 0 }
+  // ratings: {
+  //   received: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Feedback' } ],
+  //   delivered: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Feedback' } ] 
+  // },
+  // meals: {
+  //   created: {
+  //     current: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Meals', default: [] } ],
+  //     past: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Meals', default: [] } ]
+  //   },
+  //   eating: {
+  //     current: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Meals', default: [] } ],
+  //     past: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Meals', default: [] } ]
+  //   }
+  // }
 });
 
 var User = mongoose.model('Users', UserSchema);
