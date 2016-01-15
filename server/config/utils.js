ac0167db43b9a9bdad1bdaab601b1f4aad58277e
Erik Brown <erikdbrown@gmail.com>
2016-01-15 12:36:52 -0800

    added getTags function

--- server/config/utils.js
+++ server/config/utils.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var request = require('request');
+var govKey = require('./apiKeys.js');
 
 module.exports = {
   getImage: function(req, res, next) {
@@ -16,5 +18,23 @@ module.exports = {
         res.status(err.status).end();
       }
     })
+  },
+
+  getTags: function(req, res, next) {
+
+    var options = {
+      url: 'http://api.nal.usda.gov/ndb/list?format=json&lt=g&sort=n&api_key=' + givKey,
+      headers: {
+        'X-Mashape-Key': 
+      }
+    };
+
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        console.log(body)
+      } else {
+        console.log(error);
+      }
+    });
   }
 }
