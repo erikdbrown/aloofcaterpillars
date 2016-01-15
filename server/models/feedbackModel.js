f54719f8b85aa81bb0891663f6500620995cf0c9
Erik Brown <erikdbrown@gmail.com>
2016-01-14 16:05:23 -0800

    updated model to include freshness, flavor, and filling rating categories

--- server/models/feedbackModel.js
+++ server/models/feedbackModel.js
@@ -4,11 +4,11 @@ var User =  require('./userModel.js');
 
 var FeedbackSchema = mongoose.Schema({
   // _id: automatically populated by MongoDB
-  user_creator: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' }, // TODO: needs to reference the creator's ObjectID
-  user_eater: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' }, // TODO: needs to reference the eater's ObjectID
-  ratingOne: { type: Number, default: 0 }, // TODO: check in on what things we're rating
-  ratingTwo: { type: Number, default: 0 },
-  ratingThree: { type: Number, default: 0 },
+  creator: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' }, // TODO: needs to reference the creator's ObjectID
+  consumer: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' }, // TODO: needs to reference the eater's ObjectID
+  freshness: { type: Number, default: 0 }, // TODO: check in on what things we're rating
+  flavor: { type: Number, default: 0 },
+  filling: { type: Number, default: 0 },
   meal: { type: mongoose.Schema.Types.ObjectId, ref: 'Meals' } // TODO: needs to reference the meal's ObjectID
 });
 
