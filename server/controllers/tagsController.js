de32bb5cc758a6c545a4e7fd787700663278efdb
Erik Brown <erikdbrown@gmail.com>
2016-01-15 12:36:17 -0800

    added a getTags function to use USDA api route

---
+++ server/controllers/tagsController.js
@@ -0,0 +1,19 @@
+// uncomment if you need to set up a model
+// var Tag = require('../models/tagModel.js');
+var request = require('request');
+var apiKey = require('../config/apiKeys.js');
+
+module.exports = {
+  
+  getIngredients: function(req, res, next) {
+
+    request('http://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&max=1500&lt=g&api_key=' + apiKey, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        console.log(body)
+      } else {
+        console.log(error);
+      }
+    });
+  }
+
+};
