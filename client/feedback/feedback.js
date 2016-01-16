46a8bc56359ef98a2d70d2248b90466170c06ab3
Steven Law <slaw@nuclearanvil.com>
2016-01-16 13:07:31 -0800

    Properly formatted feedback page and controller.

--- client/feedback/feedback.js
+++ client/feedback/feedback.js
@@ -1,6 +1,6 @@
 angular.module('feedback', ['ngMaterial', 'ngMessages'])
 
-.controller('feedbackController', function($scope, Users) {
+.controller('feedbackController', function($scope, Users, Feedback) {
   Users.getMeals()
   .then(function(meals) {
     var pastMeals;
@@ -11,39 +11,16 @@ angular.module('feedback', ['ngMaterial', 'ngMessages'])
 
     if (pastMeals.length) {
       $scope.pastMeals = pastMeals.map(function(pastMeal) {
-        var ratingAves = rateAve(pastMeal.feedback);
-
         return {
           imgUrl: pastMeal.imgUrl,
           meal_id: pastMeal.meal_id,
           title: pastMeal.title,
           date_available: pastMeal.date_available,
-          ratings: ratingAves
+          ratings: Feedback.retrieveFeedBack(pastMeal.meal_id)
         };
       });
     } else {
       $scope.pastMeals = [];
     }
   });
-
-  var rateAve = function(feedbackArr) {
-    var averages = [];
-    var numFeedback = feedbackArr.length;
-
-    if(!numFeedback) {
-      return averages;
-    }
-
-    averages[0] = (Math.round((feedbackArr.reduce(function(a, b) {
-      return a.ratingOne + b.ratingOne;
-    })/numFeedback)*10))/10;
-    averages[1] = (Math.round((feedbackArr.reduce(function(a, b) {
-      return a.ratingTwo + b.ratingTwo;
-    })/numFeedback)*10))/10;
-    averages[2] = (Math.round((feedbackArr.reduce(function(a, b) {
-      return a.ratingOne + b.ratingOne;
-    })/numFeedback)*10))/10;
-
-    return averages;
-  };
 });
