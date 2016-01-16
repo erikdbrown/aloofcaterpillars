521e675138734afa61cd91318583d14cd63fdb70
Erik Brown <erikdbrown@gmail.com>
2016-01-15 22:05:54 -0800

    storing default image on server. factored out some comments

--- server/config/utils.js
+++ server/config/utils.js
@@ -24,25 +24,24 @@ module.exports = {
       }
 
       if (!files.picture) {
-        req.newPath = 'http://www.iconsfind.com/wp-content/uploads/2015/04/Food-Dome-icon.png';
+        req.imgPath = 'server/images/defaultMealImage.png';
         req.fields = fields;
         console.log('There are no pictures!')
         next();
       } else {
         var uniqPath = hash.encode(counter);
         counter++;
-        var path = files.picture[0].path.split('.');
+        var oldPath = files.picture[0].path.split('.');
         var ext = path[path.length - 1];
-        req.newPath = 'server/images/'+ uniqPath + '.' + ext;
+        req.imgPath = 'server/images/'+ uniqPath + '.' + ext;
         req.fields = fields;
-        fs.rename(files.picture[0].path, req.newPath, function (err) {
+        fs.rename(files.picture[0].path, req.imgPath, function (err) {
           if (err) { next(err); }
           next();
         });
       }
     });
   },
-
 
   getImage: function(req, res, next) {
     
@@ -53,29 +52,10 @@ module.exports = {
         'Content-Type' : 'image/jpeg'
       }
     }
-    console.log('Path: ', options.root)
     res.sendFile(fileName, options, function(err) {
       if (err) {
-        console.log(err);
         res.status(err.status).end();
       }
     })
   }
-  // getTags: function(req, res, next) {
-
-  //   var options = {
-  //     url: 'http://api.nal.usda.gov/ndb/list?format=json&lt=g&sort=n&api_key=' + givKey,
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   };
-
-  //   request(options, function (error, response, body) {
-  //     if (!error && response.statusCode === 200) {
-  //       console.log(body)
-  //     } else {
-  //       console.log(error);
-  //     }
-  //   });
-  }
-// }
+};
