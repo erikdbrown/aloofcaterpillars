9d5f5a33d8a5e3a19f7d1230c549211282970def
Erik Brown <erikdbrown@gmail.com>
2016-04-22 15:09:21 -0700

    Extended hashing of picture ID so that paths are not overwritten with the restart of the server

--- server/config/utils.js
+++ server/config/utils.js
@@ -28,13 +28,16 @@ module.exports = {
         req.fields = fields;
         next();
       } else {
-        var uniqPath = hash.encode(counter);
+        var hashNum = Number('' + counter + new Date().valueOf());
+        var uniqPath = hash.encode(hashNum);
         counter++;
         var oldPath = files.picture[0].path.split('.');
         var ext = oldPath[oldPath.length - 1];
-        req.imgPath = 'server/images/'+ uniqPath + '.' + ext;
+        var pathToImages = __dirname.slice(0, __dirname.length - 6) + 'images/';
+        pathToImages = pathToImages + uniqPath + '.' + ext;
+        req.imgPath = 'server/images/' + uniqPath + '.' + ext;
         req.fields = fields;
-        fs.rename(files.picture[0].path, req.imgPath, function (err) {
+        fs.rename(files.picture[0].path, pathToImages, function (err) {
           if (err) { next(err); }
           next();
         });
@@ -51,6 +54,7 @@ module.exports = {
         'Content-Type' : 'image/jpeg'
       }
     }
+
     res.sendFile(fileName, options, function(err) {
       if (err) {
         res.status(err.status).end();
